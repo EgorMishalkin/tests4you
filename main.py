394bdfe1e377af6fba124dc68f4f79556471f9e4
EgorMishalkin <57597095+EgorMishalkin@users.noreply.github.com>
2020-05-12 10:18:45 +0400

    working buttons

--- main.py
+++ main.py
@@ -9,11 +9,7 @@ app.config['SECRET_KEY'] = 'test4u_secret_key'
 def main():
     global test
     db_session.global_init("db/tests.sqlite")
-
-    test = tests.Test()
-
     session = db_session.create_session()
-    session.commit()
     test = session.query(tests.Test).all()
     # test = request.form.get(session.query(tests.Test).all(), 'test')
     app.run()
@@ -25,6 +21,31 @@ def start():
         return render_template("main_window.html", test=test)
     elif request.method == 'POST':
         return redirect('/' + request.form['button_choice_test'])
+
+
+@app.route('/<test_id>', methods=['POST', 'GET'])
+def description_test(test_id):
+    if request.method == 'GET':
+        for item in test:
+            if str(item.id) == test_id:
+                return render_template('description_test_window.html', item=item)
+            else:
+                pass
+    elif request.method == 'POST':
+        return redirect('/decision' + '/' + test_id)
+
+
+@app.route('/decision/<numb>', methods=['POST', 'GET'])
+def decision_test(numb):
+    # numb - номер теста
+    if request.method == 'GET':
+        return render_template('test_system.html', QUESTION=numb, ANSWER_1=numb,
+                               ANSWER_2=numb, ANSWER_3=numb, ANSWER_4=numb)
+    elif request.method == 'POST':
+        option = request.form['options']
+        # return option
+        return render_template('test_system.html', QUESTION=option, ANSWER_1=option,
+                               ANSWER_2=option, ANSWER_3=option, ANSWER_4=option)
 
 
 @app.route('/boys')
@@ -39,40 +60,12 @@ def girls():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')    
-  
-   
+    return render_template('about.html')
+
+
 @app.route('/easter')
 def easter():
     return render_template('easter.html')
-
-
-@app.route('/<test_id>', methods=['POST', 'GET'])
-def description_test(test_id):
-    if request.method == 'GET':
-        for item in test:
-            if str(item.id) == test_id:
-                return render_template('description_test_window.html', item=item)
-            else:
-                return render_template('error.html')
-    elif request.method == 'POST':
-        # создание запроса
-        dec = '/decision' + '/' + test_id
-        # ссылка на решение
-        return redirect(dec)
-
-
-@app.route('/decision/<numb>', methods=['POST', 'GET'])
-def decision_test(numb):
-    # numb - номер теста
-    if request.method == 'GET':
-        return render_template('test_system.html', QUESTION=numb, ANSWER_1=numb,
-                               ANSWER_2=numb, ANSWER_3=numb, ANSWER_4=numb)
-    elif request.method == 'POST':
-        option = request.form['options']
-        # return option
-        return render_template('test_system.html', QUESTION=option, ANSWER_1=option,
-                               ANSWER_2=option, ANSWER_3=option, ANSWER_4=option)
 
 
 if __name__ == '__main__':
